498a703d8ac60b91cdd5a957b838a8be79595999
Ahleeryan-Joe Ventura <ahleeryan@hotmail.com>
2026-02-19 14:16:59 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
+  # products to products/index
+  get "/products", to: "products#index"
+
 end
